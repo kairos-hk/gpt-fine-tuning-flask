a65c4664cce4c15aeed7c6f564686abdf65d1b84
DongyoungKim <kairos.hk0912@gmail.com>
2023-09-03 01:55:55 +0900

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify
 import openai
-from googletrans import Translator
 
 app = Flask(__name__)
 
 english_api_key = "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"
 english_model_name = "davinci:ft-personal-2023-09-01-13-31-52"
-translator = Translator()
+
+korean_api_key = "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"
+korean_model_name = "davinci:ft-personal-2023-09-02-15-55-41"
 
 conversation_history = []
 
@@ -19,23 +20,23 @@ def get_answer():
     if language == 'english':
         openai.api_key = english_api_key
         model_name = english_model_name
+        tokens = 40
         
     elif language == 'korean':
-        translation = translator.translate(prompt, src='ko', dest='en')
-        translated_prompt = translation.text
-
-        openai.api_key = english_api_key
-        model_name = english_model_name
-        prompt = translated_prompt + '\n' + '\n'.join(conversation_history)
+        openai.api_key = korean_api_key
+        model_name = korean_model_name
+        tokens = 140
+        
     else:
         return jsonify({"error": "Unsupported language"}), 400
 
     conversation_history.append(prompt)
+    conversation = "\n".join(conversation_history)
 
     response = openai.Completion.create(
         engine=model_name,
-        prompt=prompt, 
-        max_tokens=40,
+        prompt=conversation,
+        max_tokens=tokens,
         temperature=0.3,
         n=100
     )
