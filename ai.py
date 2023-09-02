18ab781d53df8d98d18412b038b62167ce61633e
DongyoungKim <kairos.hk0912@gmail.com>
2023-09-03 01:20:17 +0900

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,45 +1,46 @@
 from flask import Flask, request, jsonify
 import openai
-from flask_cors import CORS
+from googletrans import Translator
 
 app = Flask(__name__)
-CORS(app)
 
 english_api_key = "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"
 english_model_name = "davinci:ft-personal-2023-09-01-13-31-52"
+translator = Translator()
 
-korean_api_key = "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"
-korean_model_name = "davinci:ft-personal-2023-09-01-13-31-52"
 conversation_history = []
 
-@app.route('/gpt', methods=['POST'])
+@app.route('/get_answer', methods=['POST'])
 def get_answer():
     data = request.get_json()
     prompt = data.get('prompt')
     language = data.get('language')
 
-    if language == 'en':
+    if language == 'english':
         openai.api_key = english_api_key
         model_name = english_model_name
         
-    elif language == 'ko':
-        openai.api_key = korean_api_key
-        model_name = korean_model_name
+    elif language == 'korean':
+        translation = translator.translate(prompt, src='ko', dest='en')
+        translated_prompt = translation.text
 
+        openai.api_key = english_api_key
+        model_name = english_model_name
+        prompt = translated_prompt + '\n' + '\n'.join(conversation_history)
     else:
         return jsonify({"error": "Unsupported language"}), 400
 
     conversation_history.append(prompt)
-    conversation = "\n".join(conversation_history)
 
     response = openai.Completion.create(
         engine=model_name,
-        prompt=conversation,
-        max_tokens=30,
+        prompt=prompt, 
+        max_tokens=40,
         temperature=0.3,
         n=100
     )
+
     return jsonify({"answer": response.choices[0].text.strip()})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5003)
+    app.run(port=5003)
